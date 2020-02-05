675cb436c556b77c9dfab07d384b4784c8e2a449
gcotone <15230109+gcotone@users.noreply.github.com>
2020-02-05 14:22:53 +0100

    add missing updates

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,18 +1,12 @@
 """Test fixtures."""
 from builtins import super
 
-import pytest
-
-import yaml
-import copy
-
 from napalm.base.test import conftest as parent_conftest
-
 from napalm.base.test.double import BaseTestDouble
 
 from napalm_arubaoss.ArubaOS import ArubaOSS
 
-NAPALM_TEST_MOCK = "1"
+import pytest
 
 
 @pytest.fixture(scope='class')
@@ -27,6 +21,7 @@ def set_device_parameters(request):
     request.cls.vendor = 'arubaoss'
 
     parent_conftest.set_device_parameters(request)
+
 
 def pytest_generate_tests(metafunc):
     """Generate test cases dynamically."""
@@ -50,101 +45,17 @@ class PatchedArubaOSSDriver(ArubaOSS):
         self.device = FakeArubaOSSDevice()
 
     def is_alive(self):
+        """Mock is_alive behaviour."""
         return {"is_alive": True}  # always alive during the tests...
 
     def open(self):
+        """Mock open connection."""
         pass
 
     def close(self):
+        """Mock close connection."""
         pass
-
 
 
 class FakeArubaOSSDevice(BaseTestDouble):
     """ArubaOSS device test double."""
-    def __init__(self):
-        # self.rpc = FakeRPCObject(self)
-        # self._conn = FakeConnection(self.rpc)
-        self.alternative_facts_file = "facts.yml"
-        # self.ON_JUNOS = True  # necessary for fake devices
-        self.default_facts = {
-	    "2930f": {
-	        "out": {
-        	    "vendor": "HPE Aruba",
-	            "interface_list": [
-	                "9",
-	                "10",
-	                "1",
-	                "2",
-	                "3",
-	                "4",
-	                "5",
-	                "6",
-	                "7",
-	                "8"
-	            ],
-	            "hostname": "ansible-test",
-	            "os_version": "WC.16.09.0004",
-	            "serial_number": "CN93ABC3M1",
-	            "model": "Aruba2930F-8G-PoE+-2SFP+ Switch(JL258A)",
-	            "fqdn": "ansible-test.corp.ad.zalando.net"
-	        },
-	        "result": True,
-	        "comment": ""
-	    }
-	} 
-        self._uptime = 4380
-
-    @property
-    def facts(self):
-        # we want to reinitialize it every time to avoid side effects
-        self._facts = copy.deepcopy(self.default_facts)
-        try:
-            alt_facts_filepath = self.find_file(self.alternative_facts_file)
-        except IOError:
-            self._facts = self.default_facts
-            return self._facts
-        with open(alt_facts_filepath, "r") as alt_facts:
-            self._facts.update(yaml.safe_load(alt_facts))
-        return self._facts
-
-    @property
-    def uptime(self):
-        return self._uptime
-
-    def open(self):
-        pass
-
-    def close(self):
-        pass
-
-    def bind(*args, **kvargs):
-        pass
-
-    def cli(self, command=""):
-        """Fake CLI method."""
-        ret = dict()
-
-        for command in command_list:
-            filename = "{safe_command}.txt".format(safe_command=self.sanitize_text(command))
-            filepath = self.find_file(filename)
-        
-            ret[self.sanitize_text(command)] = self.read_txt_file(filepath)
-
-        return ret
-
-    def run_commands(self, command_list, encoding='json'):
-        """Fake run_commands."""
-        result = list()
-
-        for command in command_list:
-            filename = '{}.{}'.format(self.sanitize_text(command), encoding)
-            full_path = self.find_file(filename)
-
-            if encoding == 'json':
-                result.append(self.read_json_file(full_path))
-            else:
-                result.append({'output': self.read_txt_file(full_path)})
-
-        return result
-
